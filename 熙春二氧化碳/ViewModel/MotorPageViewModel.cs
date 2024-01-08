690563f9be7016cdd7c48868a86ab138fb3247cf
1271461167 <1271461167@qq.com>
2024-01-08 10:32:21 +0800

    校正电机回零

--- 熙春二氧化碳/ViewModel/MotorPageViewModel.cs
+++ 熙春二氧化碳/ViewModel/MotorPageViewModel.cs
@@ -154,7 +154,7 @@ namespace _2023_12_11XiChun.ViewModel
             home.velLow = 0.5;
             home.acc = 0.1;
             home.dec = 0.1;
-            home.searchHomeDistance = 150000;
+            home.searchHomeDistance = 200000;
             home.searchIndexDistance = 15000;
             home.escapeStep = 1000;
             sRtn = gts.mc.GT_GoHome(1, ref home);
@@ -163,7 +163,7 @@ namespace _2023_12_11XiChun.ViewModel
             {
                 do
                 {
-                    // 读取2轴的状态
+                    // 读取1轴的状态
                     sRtn = gts.mc.GT_GetHomeStatus(1, out homeStatus);
                     if (sRtn != 0)
                     {
