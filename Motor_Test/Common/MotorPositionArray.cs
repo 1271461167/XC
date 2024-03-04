449fa3e76fe67eb1d23b6753ef4dc7ea09be1623
1271461167 <1271461167@qq.com>
2024-03-04 17:29:44 +0800

    电机阵列

---
+++ Motor_Test/Common/MotorPositionArray.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Motor_Test.Common
+{
+    public class MotorPositionArray
+    {
+        public static int Row {  get; set; }
+        public static int Column { get; set; }
+        public static double RowSpacing {  get; set; }
+        public static double ColumnSpacing { get; set; }
+        public static Point StartPoint { get; set; }
+        public List<List<Point>> Points { get; set;}=new List<List<Point>>();
+    }
+}
