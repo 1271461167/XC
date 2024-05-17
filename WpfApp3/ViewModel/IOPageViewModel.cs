013c0981ac5b3613d173537e186ca28c3c24fcee
1271461167 <1271461167@qq.com>
2024-05-17 17:35:20 +0800

    IO界面更新

--- WpfApp3/ViewModel/IOPageViewModel.cs
+++ WpfApp3/ViewModel/IOPageViewModel.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Markup;
+using System.Windows.Threading;
 using WpfApp3.Common;
 using WpfApp3.Common.LMC;
 
@@ -13,26 +16,44 @@ namespace WpfApp3.ViewModel
     public class IOPageViewModel
     {
         IMarkController _markController = LMC.GetInstance();
-        public ObservableCollection<bool> INS { get; set; } = new ObservableCollection<bool>() {false, false, false, false, false, false, false, false};
-        public ObservableCollection<bool> OUTS { get; set; } = new ObservableCollection<bool>() {false,false ,false, false, false, false, false, false};
+        public CommandBase CloseCommand { get; set; } = new CommandBase();
+        public CancellationTokenSource CancellationTokenSource { get; set; } = new CancellationTokenSource();
+        public ObservableCollection<bool> INS { get; set; } = new ObservableCollection<bool>() { false, false, false, false, false, false, false, false };
+        public ObservableCollection<bool> OUTS { get; set; } = new ObservableCollection<bool>() { false, false, false, false, false, false, false, false };
+
         public IOPageViewModel()
         {
-            GetIOState();
+            Task.Run(() => { GetIOState(CancellationTokenSource.Token); }, CancellationTokenSource.Token);
+            CloseCommand.DoCanExecute = new Func<object, bool>((obj) => { return true; });
+            CloseCommand.DoExecute = new Action<object>((obj) =>
+            {
+                CancellationTokenSource?.Cancel();
+                CancellationTokenSource.Dispose();
+                CancellationTokenSource = null;
+            });
         }
-        private void GetIOState()
+
+
+        private void GetIOState(CancellationToken token)
         {
-            ushort outs = 0;
-            ushort ins = 0;
-            _markController.GetOutPort(ref outs);
-            _markController.ReadPort(ref ins);
-            for(int i=0;i<OUTS.Count;i++)
+            while (true)
             {
-                OUTS[i]=((outs&(0x01<<i))!=0)?true:false;
+                if (token.IsCancellationRequested)
+                {
+                    break;
+                }
+                ushort outs = 0;
+                ushort ins = 0;
+                _markController.GetOutPort(ref outs);
+                _markController.ReadPort(ref ins);
+                for (int i = 0; i < INS.Count; i++)
+                {
+                    INS[i] = true;
+                }
+
             }
-            for (int i = 0; i < INS.Count; i++)
-            {
-                INS[i] = ((ins & (0x01 << i)) != 0) ? true : false;
-            }
+
         }
+
     }
 }
